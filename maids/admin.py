e011e015d9258065c9dce621cebe22b99089a1fd
Rakii234 <raki.goud234@gmail.com>
2023-09-13 12:45:04 +0530

    first commit

---
+++ maids/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from maids.models import *
+
+# Register your models here.
+admin.site.register(Maid)
+admin.site.register(AvailabilitySlot)
+admin.site.register(Review)
